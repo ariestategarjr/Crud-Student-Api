4c589f10f61f6de9a3bad15d826d8a07b73cb05a
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-12-26 19:09:26 +0700

    setup index.js, connection.js, and install nodemon

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const db = require('./connection');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
 
+app.get('/', (req, res) => {
+    res.send(`Hello World!`);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
