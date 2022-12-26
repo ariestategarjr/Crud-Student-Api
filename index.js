9617a0299bc249836b70f71cc50d77d8e89accf2
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-12-26 20:09:50 +0700

    update index.js make end-point / and /search

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const db = require('./connection');
 const bodyParser = require('body-parser');
+const response = require('./response');
 const app = express();
 const port = 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     const sql = `SELECT * FROM student`;
     db.query(sql, (error, result) => {
         if (error) throw error;
         const data = JSON.parse(JSON.stringify(result));
-        // console.log(data);
-        // res.send(data);
+        response(200, data, 'get all data', res);
     }); 
+});
+
+app.get('/search', (req, res) => {
+    const sql = `SELECT alamat FROM student WHERE nim = ${req.body.nim}`;
+    db.query(sql, (error, result) => {
+        if (error) throw error;
+        // const data = JSON.parse(JSON.stringify(result));
+        // console.log(result);
+        response(200, result, 'search alamat by name', res);
+    });
 });
 
 app.listen(port, () => {
