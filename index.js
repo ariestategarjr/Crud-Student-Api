2a6895253a3e354711cedf61782bae0628461fa1
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-12-27 21:46:26 +0700

    update index.js make method post route path /student

--- index.js
+++ index.js
@@ -31,6 +31,21 @@ app.get('/student/:nim', (req, res) => {
     });
 });
 
+app.post('/student', (req, res) => {
+    const { nim, nama, kelas, alamat } = req.body;
+    const sql = `INSERT INTO student VALUES (NULL, ${nim}, '${nama}', '${kelas}', '${alamat}')`;
+    db.query(sql, (error, result) => {
+        if (error) response(500, "error", "insert data invalid", res);
+        if (result?.affectedRows) {
+            const data = {
+                isSuccess: result.affectedRows,
+                id: result.insertId
+            };
+            response(200, data, "insert data ok", res);
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
